6a20b4cc1a097e94db868c12e67df7382755f645
Amirhosein Ataei <ataeiamirhosein@gmail.com>
2020-04-27 23:55:41 +0200

    Add files via upload

--- sar_application_NFV_client.py
+++ sar_application_NFV_client.py
@@ -1,45 +1,35 @@
-#!/usr/bin/python2
-
 from scapy.all import *
-import time 
+import time
 from datetime import datetime
 from threading import Thread
 
+file1 = open('tosearch1.txt', 'r') 
+my_list = file1.readlines() 
+
+
 
 class MonitoringThread(Thread):
-	def __init__(self, src, dst, tos, iface):
+	
+	def __init__(self, src, dst, iface):
 		Thread.__init__(self)
 		self.src = src
 		self.dst = dst
-		self.tos = tos
 		self.iface = iface
-		self.max_number_pkts = 10		
-		self.given_time = 1
-		self.start_time=time.time()
-		self.timestamp_origin = 0
-		self.flag = 0
-
+	
 	def monitoring_app(self):
-		while True:
-			actual_time = int((time.time() - self.start_time) % 60.0)					
-			if actual_time == self.given_time:
-				print actual_time, self.given_time, self.iface
-				if self.flag == 0:
-					self.timestamp_origin = time.time()
-					self.flag = 1					
-				data = str(time.time())
-				send(IP(src=self.src, dst=self.dst, tos=self.tos)/ICMP()/data) 
-
-				self.given_time = self.given_time + 1
-				if self.given_time == 60:
-					self.given_time = 1
-
+		for entry in my_list:
+			# binary_address = entry.split(',')[1]
+			data = entry# str(binary_address)
+			# print (data)
+			send(IP(dst=self.dst)/UDP(sport=1610,dport=1610)/data)
+		
+		# while 1:
+			
+		# 	input1 = input()
+		# 	send(IP(dst=self.dst)/UDP(sport=1610,dport=1610)/str(input1))
 
 	def run(self):
-		conf.L3socket = L3RawSocket
 		self.monitoring_app()
 
-
-thread2 = MonitoringThread("10.0.0.3", "10.0.0.2", 4, "enp0s3")
+thread2 = MonitoringThread("10.0.0.3", "10.0.0.2", "enp0s3")
 thread2.start()
-
--- sar_application_NFV_server.py
+++ sar_application_NFV_server.py
@@ -1,4 +1,3 @@
-
 from collections import Counter
 from scapy.all import *
 import time 
@@ -8,11 +7,17 @@ import numpy as np
 import csv
 import os.path
 from os import path
+import BinaryNode
+import MultibitNode
+import timeit
+times = []
 
 
-stats_1 = []
-stats_2 = []
+binary_root = BinaryNode.Create("0")
+multibit_root = MultibitNode.Create()
 
+sum_time = 0
+# 888888888888888888888888888888888888888888888888888888888888
 class MonitoringThread(Thread):
 	def __init__(self, src, dst, iface):
 		Thread.__init__(self)
@@ -20,24 +25,75 @@ class MonitoringThread(Thread):
 		self.dst = dst
 		self.iface = iface
 		print 'Ready to monitoring'
+		print '>>>>>>>>>>>>>>>>>>>'
+		print '<<<<<<<<<<<<<<<<<<<'
 
+# 8888888888888888888888888888888888888888888888888888888888888
+	def convert_in_bin(self, address):
+		return ''.join([bin(int(x)+256)[3:] for x in address.split('.')])
+# 8888888888888888888888888888888888888888888888888888888888888
+	def binary_lookup(self, packet):
+		global sum_time
 
-	def custom_action(self, packet):
+		# start = timeit.default_timer()
+	
 		if "IP" in packet:
-			timestamp_arrival = time.time()
-			src = packet["IP"].src
-			dst = packet["IP"].dst
-			data = float(packet["Raw"].load)
-			delay = timestamp_arrival - data
-			print src, dst, delay
+			# timestamp_arrival = time.time()
+			data = (packet["Raw"].load).split(',')[1]
+			# print ("Lookup request received for : " + str(data))#src, dst, data
+			#data1 = str(data)
+			# print ("Longest prefix found, IP is = " + str(binary_root.Lookup(self.convert_in_bin(data), "0")))		
+			start_time = time.time()
+			binary_root.Lookup(self.convert_in_bin(data), "0")
+			end_time = time.time()
+			sum_time = sum_time + (end_time - start_time)
+			print("--- %s Single Lookup ---" % (end_time - start_time))
+			print("--- %s Sum of all Lookups ---" % sum_time)		
+		# end = timeit.default_timer() - start
+		# times.append(end * 1000)
+		# print("binary_trie (avg per lookup): " + str(sum(times)/len(times)) + "	ms")
+		# print("binary_trie (sum of all times): " + str(sum(times)) + "	ms")
+		print ("=========================================================")
+# 88888888888888888888888888888888888888888888888888888888888888888
+	def multibit_lookup(self, packet):
+		global sum_time
+		# sum_time = 0
+		# start = timeit.default_timer()
 
+		
+		# main()
+		
+		
 
+		if "IP" in packet:
+			# timestamp_arrival = time.time()
+			data = (packet["Raw"].load).split(',')[1]			
+			# print ("Lookup request received for : " + str(data))#src, dst, data
+			#data1 = str(data)
+			
+			# print ("Longest prefix found, IP is = " + str(multibit_root.LookupNonRecursive(self.convert_in_bin(data), "0")))
+		
+			start_time = time.time()
+			multibit_root.LookupNonRecursive(self.convert_in_bin(data), "0")
+			end_time = time.time()
+			sum_time = sum_time + (end_time - start_time)
+			print("--- %s Single Lookup ---" % (end_time - start_time))
+			print("--- %s Sum of all Lookups ---" % sum_time)	
+
+		# end = timeit.default_timer() - start
+		# times.append(end * 1000)
+		# # print("multibit_trie (avg per lookup): " + str(sum(times)/len(times)) + "	ms")
+		# # print("multibit_trie (sum of all times): " + str(sum(times)) + "	ms")
+		print ("=========================================================")
+# 888888888888888888888888888888888888888888888888888888888888888888888
 	def run(self):
-		global stats_1
-		global stats_2
-		conf.L3socket = L3RawSocket
-		sniff(filter="icmp", iface=self.iface, prn=self.custom_action)
-
-
+		print "Select \n 1- Binary lookup \n 2- Multibit lookup :"
+		input_choose = input()
+		print "Waiting for incoming request ..."
+		if input_choose == 2:
+			sniff(filter="udp", iface=self.iface, prn=self.multibit_lookup)
+		if input_choose == 1:
+			sniff(filter="udp", iface=self.iface, prn=self.binary_lookup)
+# 888888888888888888888888888888888888888888888888888888888888888888888
 thread1 = MonitoringThread("10.0.0.3", "10.0.0.2", "enp0s3")
 thread1.start()
